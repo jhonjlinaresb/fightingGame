a90f897d056b074c039c2fe8acb60634e18b4b94
jhonjlinaresb <jhonjlinaresb@gmail.com>
2020-09-22 11:22:44 +0200

    Creación de Arrays en JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 let pantalla1 = document.getElementById("fase1");
 let pantalla2 = document.getElementById("fase2");
 let pantalla3 = document.getElementById("fase3");
+
+const cambioPantalla = (valor) => {
+
+    //Ahora se a que pantalla quiero dirigirme a concatenar fase + valor
+    //por parámetro
+    let faseDestino = "fase" + valor;
+
+    //A continuación creo un Array con trodas las fases
+    let arrayFases = ["fase1", "fase2", "fase3"];
+
+    arrayFases = arrayFases.filter(val => !faseDestino.includes val);
+
+}    
